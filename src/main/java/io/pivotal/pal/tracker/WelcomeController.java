4faa2c92e54e19137072e692449d74447f6e4bab
telecomwireless <kcnagareddy@gmail.com>
2020-11-03 21:18:10 +0000

    actuators endpoints

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
-    private String yourwelcomemessage;
+    private final String yourwelcomemessage;
 
 
     public WelcomeController(@Value("${welcome.message}") String yourwelcomemessage) {
